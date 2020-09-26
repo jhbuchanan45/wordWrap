ced0f9e720dc36edab7aa14315af11e0f018206a
jhbuchanan45 <jbuchanan21@qub.ac.uk>
2020-09-26 22:59:59 +0100

    Initial commit with array method

---
+++ src/com/jacob/wordwrap/Main.java
@@ -0,0 +1,50 @@
+package com.jacob.wordwrap;
+
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        char[] input;
+        char[][] wrapArr;
+        int wordLen;
+
+        System.out.println("Enter a single word");
+
+        //grab input and convert to char array
+        input = scanner.nextLine().toCharArray();
+
+        if (input.length < 1) {
+            System.out.println("You entered nothing, so no output to display.");
+            return;
+        }
+
+        //grab word length and initialise wrapArr for filling
+        wordLen = input.length;
+        wrapArr = new char[wordLen][wordLen];
+
+        for (int i=0, j=wordLen-1; i < wordLen; i++, j--) {
+            //first fill the row with spaces if not on first or last row
+            if (j != 0) {
+                Arrays.fill(wrapArr[i], ' ');
+            }
+
+            //now fill the first and last column with the correct letter
+            wrapArr[i][0] = input[i];
+            wrapArr[i][wordLen-1] = input[j];
+
+            //fill the first row from input simultaneously
+            wrapArr[0][i] = input[i];
+
+            //and fill the last row backwards from input simultaneously
+            wrapArr[wordLen-1][i] = input[j];
+        }
+
+        //print out each row into the console
+        for (char[] c : wrapArr) {
+            System.out.println(c);
+        }
+    }
+}
