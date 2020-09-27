88e75543698a30eff4e320c1278c6e9e78924f4c
jhbuchanan45 <jbuchanan21@qub.ac.uk>
2020-09-27 14:18:33 +0100

    Add .gitignore and no arrays version

--- src/com/jacob/wordwrap/Main.java
+++ src/com/jacob/wordwrap/Main.java
@@ -1,50 +1,49 @@
 package com.jacob.wordwrap;
 
 import java.util.Scanner;
-import java.util.Arrays;
 
 public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        char[] input;
-        char[][] wrapArr;
         int wordLen;
+        String input;
+        String spaces;
 
         System.out.println("Enter a single word");
+        //get user input
+        input = scanner.nextLine();
+        wordLen = input.length();
 
-        //grab input and convert to char array
-        input = scanner.nextLine().toCharArray();
-
-        if (input.length < 1) {
+        //check for input presence
+        if (wordLen < 1) {
             System.out.println("You entered nothing, so no output to display.");
-            return;
         }
+        else if (wordLen == 1) {
+/           //handle case of single letter
+            System.out.println("You en");
+        }
+        else {
+            //get number of spaces for middle lines
+            spaces = " ".repeat(wordLen - 2);
 
-        //grab word length and initialise wrapArr for filling
-        wordLen = input.length;
-        wrapArr = new char[wordLen][wordLen];
-
-        for (int i=0, j=wordLen-1; i < wordLen; i++, j--) {
-            //first fill the row with spaces if not on first or last row
-            if (j != 0) {
-                Arrays.fill(wrapArr[i], ' ');
+            //main loop to print to console line by line
+            for (int i=0, j=wordLen-1; i < wordLen; i++, j--) {
+                if (i==0) {
+                    //on first line print word
+                    System.out.println(input);
+                }
+                else if (j==0) {
+                    //on last line print word reversed
+                    for (int k=wordLen-1; k >= 0; k--) {
+                        System.out.print(input.charAt(k));
+                    }
+                }
+                else {
+                    //on any other line print letter, then spaces, then opposite
+                    System.out.println(input.charAt(i) + spaces + input.charAt(j));
+                }
             }
-
-            //now fill the first and last column with the correct letter
-            wrapArr[i][0] = input[i];
-            wrapArr[i][wordLen-1] = input[j];
-
-            //fill the first row from input simultaneously
-            wrapArr[0][i] = input[i];
-
-            //and fill the last row backwards from input simultaneously
-            wrapArr[wordLen-1][i] = input[j];
-        }
-
-        //print out each row into the console
-        for (char[] c : wrapArr) {
-            System.out.println(c);
         }
     }
 }
